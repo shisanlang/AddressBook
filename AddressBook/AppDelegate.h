9067d9e487a492471d84ae2a9f4fd0476850b38b
shisanlang <shisanlangd@gmail.com>
2013-09-26 12:14:10 +0800

    create addressbook

---
+++ AddressBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by duan on 13-9-26.
+//  Copyright (c) 2013年 duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AddressBookTests/AddressBookTests.m
@@ -0,0 +1,34 @@
+//
+//  AddressBookTests.m
+//  AddressBookTests
+//
+//  Created by duan on 13-9-26.
+//  Copyright (c) 2013年 duan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AddressBookTests : XCTestCase
+
+@end
+
+@implementation AddressBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
